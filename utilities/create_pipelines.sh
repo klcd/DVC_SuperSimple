3041a63146700e402d3fa8be3b0254981dd06cf1
Cedric Klinkert <cedric.klinkert@bbv.ch>
2022-12-30 19:32:24 +0100

    Add scripts to dependencies of pipeline
    - handle initial data in seperate folder

--- utilities/create_pipelines.sh
+++ utilities/create_pipelines.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 dvc run --name split \
- --deps data/data.csv \
+ --deps stages/split.py \
+ --deps initial_data/data.csv \
  --outs data/test_data.csv \
  --outs data/train_data.csv \
  --params split.param \
  python stages/split.py --config params.yaml
 
 dvc run --name train \
+ --deps stages/train.py \
 --deps data/train_data.csv \
 --outs models/model_mlem \
 --params train.param \
 python stages/train.py --config params.yaml
 
 dvc run --name evaluate \
+ --deps stages/evaluate.py \
 --deps data/test_data.csv \
 --deps data/train_data.csv \
 --deps models/model_mlem \
