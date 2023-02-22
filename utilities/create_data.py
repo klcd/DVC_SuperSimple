a4fe39b681b0667b0c282c8ad34ec64e4fe4505b
Cedric Klinkert <cedric.klinkert@bbv.ch>
2023-02-22 16:59:40 +0000

    fix create data

--- utilities/create_data.py
+++ utilities/create_data.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pandas as pd
+import os
 
 N = 100
 x = np.linspace(0, 2*np.pi,N)
 
 #Data = a*x + b + noise
-y = np.random.uniform(-5,5,1)*x+np.random.uniform(-5,5,1)
-noise = np.random.normal(0,.7, N)
+y = 4*x+1
+noise = np.random.normal(0,1, N)
+
+home = '/workspaces/DVC_SuperSimple'
 
 df = pd.DataFrame({'x':x, 'y':y+noise})
-df.to_csv('./data/data.csv', index=False)
+df.to_csv(os.path.join(home,'initial_data','data.csv'), index=False)
 
 df = pd.DataFrame({'x':x, 'y':y})
-df.to_csv('./data/underlying_data.csv', index=False)
+df.to_csv(os.path.join(home,'initial_data','underlying_data.csv'), index=False)
